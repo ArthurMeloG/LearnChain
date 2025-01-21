f13b1ffe59ccc976ee1357dc769c98304969cee1
ArthurMeloG <arthurdemelo2001@gmail.com>
2025-01-21 17:11:08 -0300

    tipagem

---
+++ src/app/types/course.ts
@@ -0,0 +1,23 @@
+export interface CourseTopic {
+    id: string;
+    title: string
+    subtitle: string
+    description: string
+    author: string
+    date: string
+    duration: string
+    views: string
+    courseId: string
+}
+
+export interface Course {
+    id: string;
+    title: string
+    subtitle: string
+    description: string
+    author: string
+    date: string
+    duration: string
+    views: string
+}
+
